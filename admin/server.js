6f315b972fe4bc31d6741ca50f38fb9a828d7e13
Priyankaabuj <it.a.01.priyanka.abuj@gmail.com>
2023-10-11 20:16:35 +0530

    admin-panel-established

---
+++ admin/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB Connection
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://bloodbank:bloodbank0135@cluster0.dzykr27.mongodb.net/'; // Update with your MongoDB connection URI
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
+// API Routes
+const adminRoutes = require('./routes/admin');
+app.use('/api/admin', adminRoutes);
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong on the server' });
+});
+
+// Serve React Frontend (build) if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
+// Start Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
